02f08717d718bc68c9ee50d88a17534872b71963
Matheus Vieira <matheussantosbmth@gmail.com>
2026-02-24 22:13:14 -0300

    Aula 1 - Variáveis em JavaScript

---
+++ Aula1/src/script.js
@@ -0,0 +1,21 @@
+console.log("Testando JavaScript");
+
+// Exemplos de variável let
+let nome = 'Matheus';
+let nome2 = 'Vieira';
+
+console.log(nome);
+console.log(nome2);
+console.log(nome, nome2);
+
+// Exemplos de variável const
+const times = ['São Paulo', 'Manchester United', 'AC Milan'];
+// const time = 'São Paulo';
+// const time2 = 'Manchester United';
+// const time3 = 'AC Milan';
+
+console.log(times);
+console.log(times[0]);
+// console.log(time);
+// console.log(time2);
+// console.log(time3);
