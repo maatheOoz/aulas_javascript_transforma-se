b69febfc6e45f7e55a987c6337f2f2c0666b64dc
Matheus Vieira <matheussantosbmth@gmail.com>
2026-02-25 22:17:55 -0300

    Aula 2 - Operadores

---
+++ Aula2/src/script02.js
@@ -0,0 +1,29 @@
+// Exemplo de operação aritmética
+let a = 5;
+let x = (100 + 200) * a;
+
+console.log(x);
+
+// Exemplo de adição
+let texto1 = "Somente a dor coletiva ";
+
+// texto1 = texto1 + 'gera a união!';
+texto1 += 'gera a união!';
+
+console.log(texto1);
+
+// Exemplo de Incremento
+let w = 5;
+w++;
+w++;
+let z = w;
+
+console.log(z);
+
+// Exemplo de Decremento
+let b = 5;
+b--;
+b--;
+let c = b;
+
+console.log(c);
