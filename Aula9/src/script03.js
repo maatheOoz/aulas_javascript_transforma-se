3d7078e2eddf8ba8ccd74f083e8bffe1d27944c0
Matheus Vieira <matheussantosbmth@gmail.com>
2026-03-09 21:13:39 -0300

    Aula 9 - Aprendendo BigInt em JavaScript

---
+++ Aula9/src/script03.js
@@ -0,0 +1,29 @@
+// Utilizando o BigInt
+let a = BigInt("1651654165416515616545613484167485165497841564868541");
+
+console.log(a);
+
+// Utilizando n
+let b = 5165215648561654189489541564165418514568418956418541n;
+
+console.log(b);
+
+// Utilizando operadores aritméticos com BigInt
+let c = 131231321654146541651651561n;
+let d = 131231321654146541651651561n;
+let e = (c * d);
+
+console.log(e);
+
+// Convertendo BigInt
+let f = BigInt("516156419196515615135151651651");
+let fConvertido = Number(f);
+
+console.log(fConvertido);
+
+// Utilizando métodos de comparação em BigInt
+let g = 100n;
+let h = 100n;
+let exComparacao = (g === h);
+
+console.log(exComparacao);
