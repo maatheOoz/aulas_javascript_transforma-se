1ff44a4240a53dd5e10500d0ff728f9a7e125c70
Matheus Vieira <matheussantosbmth@gmail.com>
2026-03-19 22:12:56 -0300

    Aula 16 - Exemplo de animação

--- Aula16/src/script03.js
+++ Aula16/src/script03.js
@@ -1,7 +1,7 @@
 // Validação de Formulário
-const formulario = document.forms["novoForm"]["texto"].value;
-
 function validarForm() {
+    let formulario = document.forms["novoForm"]["texto"].value;
+    
     if (formulario == "") {
         alert("Por favor, insira um valor para o campo!");
         return false;
